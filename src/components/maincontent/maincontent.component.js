9de3329057930f432602beab34a73f88467c1c6e
Asif <asifaly310@gmail.com>
2021-03-01 11:40:12 +0500

    usip 1 using html css

---
+++ src/components/maincontent/maincontent.component.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './main.css';
+
+const Maincontent = () => {
+return(
+    <div className='main-content'>
+         <h1>MainContent</h1>
+    </div>
+)
+
+
+}
+export default Maincontent;
---
+++ src/components/sidebar/sidebar.component.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './sidebar.css';
+
+const Sidebar = () => {
+
+    return(
+        <div className='sidenav'>
+           <h1>Sidebar</h1> 
+        </div>
+    )
+
+}
+
+export default Sidebar;
